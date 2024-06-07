0eb60f369343699292baa8be76d14818677b7c06
olchia <olyaburtseva@me.com>
2024-06-07 14:05:36 +0300

    add new const

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import pygame
 
 pygame.init()
 
+# Создаем экран
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load()
+
 # При создании игры всегда необходимо создавать игровой цикл.
 # Делаем это через while. Можно было сделать бесконечный цикл while, но из него было бы не так просто выйти.
 # Поэтому мы делаем через переменную.
